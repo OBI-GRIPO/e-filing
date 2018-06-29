a28da320ddb4da0aef3d3bfb0ef29e840ea94ff6
sl45sms <sl45sms@yahoo.gr>
2018-06-29 11:39:40 +0300

    ousting wso2

--- infrastructure/build.sh
+++ infrastructure/build.sh
@@ -2,6 +2,6 @@
 ./up.py $1 | docker-compose -f - build sorl
 ./up.py $1 | docker-compose -f - build libreoffice
 ./up.py $1 | docker-compose -f - build share
-./up.py $1 | docker-compose -f - build wso2
+
 
 
