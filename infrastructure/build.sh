e7a912760acd23b3bbe80e750dc1207bf4916f8f
sl45sms <sl45sms@yahoo.gr>
2018-07-12 18:22:35 +0300

    add pdf extractor

--- infrastructure/build.sh
+++ infrastructure/build.sh
@@ -2,6 +2,7 @@
 ./up.py $1 | docker-compose -f - build sorl
 ./up.py $1 | docker-compose -f - build libreoffice
 ./up.py $1 | docker-compose -f - build share
+./up.py $1 | docker-compose -f - build fio2pdf
 
 
 
