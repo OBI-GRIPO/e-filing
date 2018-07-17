4d3e75a298854f7412b764d5a3b3e8d30f6df5d5
sl45sms <sl45sms@yahoo.gr>
2018-07-17 18:41:34 +0300

    fix typo

--- infrastructure/build.sh
+++ infrastructure/build.sh
@@ -1,5 +1,5 @@
 ./up.py $1 | docker-compose -f - build alfresco
-./up.py $1 | docker-compose -f - build sorl
+./up.py $1 | docker-compose -f - build solr
 ./up.py $1 | docker-compose -f - build libreoffice
 ./up.py $1 | docker-compose -f - build share
 ./up.py $1 | docker-compose -f - build fio2pdf
